99d8273d0435e2cef6510245cd10dd3a25ad104e
Bahlul19 <bahlul.tausif@gmail.com>
2021-08-18 17:30:26 +0600

    Working with ChartDataController with some functionality

--- app/Http/Controllers/ChartDataController.php
+++ app/Http/Controllers/ChartDataController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ChartDataModel;
 
 class ChartDataController extends Controller
 {
-    //
+    function onAllSelect()
+    {
+        $result = ChartDataModel::all();
+        return $result;
+    }
 }
